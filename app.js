995cdff467347084370b6c3ea63236c896b7d4ec
Theinniy <thiy.amorim@gmail.com>
2025-03-08 16:00:26 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let guardarNomeAmigos = [];
+
+function adicionarNomes() {
+    let input = document.getElementById ('amigo');
+    let nome = input.value.trim();
+    
+}
